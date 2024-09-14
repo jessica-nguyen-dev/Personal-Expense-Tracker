96ca59fcd3ed7c00c80a1d7e7ed3b3c5feeaefc1
jessica-nguyen-dev <jessica.u.nguyen@sjsu.edu>
2024-09-13 21:24:26 -0700

    Added user input logic to collect data on category, expense name, and price. Ensures that input can only be from 1 to 6, or the amount of the categories list

--- expense_tracker.py
+++ expense_tracker.py
@@ -15,6 +15,36 @@ def main(): # when the main function is run it will run all the below functions 
 
 def get_user_expense():
     print(f"📝 Getting User Expense!")
+    expense_name = input("Enter expense name: ")
+    expense_amount = float(input("Enter expense amount: "))
+    print(f"You've entered {expense_name}, {expense_amount}.")
+
+    # list of categories for user to choose from
+    expense_categories = [
+        "🍣 Food",
+        "🏋️ Fitness",
+        "✏️ School",
+        "🏖️ Fun",
+        "🚗 Transportation",
+        "🏷️ Misc"
+    ]
+
+    while True:
+        print("Select a category: ")
+        element_index = 1
+        for element in expense_categories:
+            print(f"{element_index}. {element}")
+            element_index += 1
+
+        value_range = f"[1 - {len(expense_categories)}]"
+        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1
+
+        if selected_index in range(len(expense_categories)):
+            break
+        else:
+            print("Invalid category. Please try again!")
+
+
 
 def save_expense_to_file():
     print(f"📂 Saving User Expense!")
