3ee4dfbc795d3845b96e01ba4d2644a27d52f61f
jessica-nguyen-dev <jessica.u.nguyen@sjsu.edu>
2024-09-13 23:15:16 -0700

    New addition to last function determines todays date and calculates how many are left in the month. With that we can calculate how much you can spend a day for the rest of the month without going over the budget.

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,8 +1,11 @@
 from expense import Expense # refers to expense class
+import calendar
+from datetime import datetime
 
 def main(): # when the main function is run it will run all the below functions in order
     print(f"Running Expense Tracker!")  # checks that main is running properly
     expense_file_path = "expenses.csv"  # creating the csv file
+    budget = 2000
 
     # Each part will be a function to simplify the code and allow for easier debugging
 
@@ -13,7 +16,7 @@ def main(): # when the main function is run it will run all the below functions 
     save_expense_to_file(expense, expense_file_path)
 
     # Read file and summarize expenses.
-    summarize_expenses(expense_file_path)
+    summarize_expenses(expense_file_path, budget)
 
 
 def get_user_expense():
@@ -54,30 +57,42 @@ def save_expense_to_file(expense, expense_file_path):
     with open(expense_file_path, "a", encoding="utf-8") as f: # Opens a file for appending data.  If file does not exist, it creates a one.
         f.write(f"{expense.name},{expense.amount},{expense.category}\n") # Writes a line of text to the file
 
-def summarize_expenses(expense_file_path):
+def summarize_expenses(expense_file_path, budget):
     print(f"📊 Summarizing User Expense!")
-    expenses = []
+    expenses = [] # list of objects
     with open(expense_file_path, "r", encoding="utf-8") as f: # r stands for read only mode
-        lines = f.readlines()   # creates a list of strings where each item corresponds to a line in the CSV file
+        lines = f.readlines()   # Reads all lines from the file into a list, where each line is a string
         for line in lines:
             expense_name, expense_amount, expense_category = line.strip().split(",") # strips then splits line into 3
-            line_expense = Expense(
+            line_expense = Expense(     # Creates an Expense object using the extracted values.
                 name=expense_name, amount=float(expense_amount), category=expense_category
-            )
+            )       # line_expense is a variable that holds an instance (or object)  of the Expense class.
             expenses.append(line_expense) # once file is read, append to expenses list
 
     amount_by_category = {}     # creating a dictionary
-    for expense in expenses:
-        key = expense.category
-        if key in amount_by_category:
-            amount_by_category[key] += expense.amount
+    for expense in expenses:    # Iterates over each Expense object in the expenses list.
+        key = expense.category  # Uses the category of the current expense as the dictionary key.
+        if key in amount_by_category:   # Checks if the category is already in the dictionary.
+            amount_by_category[key] += expense.amount # Each key is unique within the dictionary, and each key is associated with a value
         else:
             amount_by_category[key] = expense.amount
 
     print("Expenses By Category: ")
-    for key, amount in amount_by_category.items():
-        print(f"{key}: ${amount:.2f}")
+    for key, amount in amount_by_category.items():  # Iterates over each key-value pair in the dictionary.
+        print(f"    {key}: ${amount:.2f}")              # Dictionaries have a key and an associated value, which is why we say for key and amount
 
+    total_spent = sum([expense.amount for expense in expenses]) # for every expense in the expense list, add item to a new list that is the expense amount, then sum it
+    print(f"💰 Total spent: ${total_spent:.2f}")
+
+    remaining_budget = budget - total_spent
+    print(f"✅ Budget Remaining: ${remaining_budget:.2f}")
+
+    now = datetime.now()
+    days_in_month = calendar.monthrange(now.year, now.month)[1]
+    remaining_days = days_in_month - now.day
+
+    daily_budget = remaining_budget/remaining_days
+    print(f"👉 Budget Per Day: ${daily_budget:.2f}")
 
 
 # ensures that the code runs only when we run this file, not when in other files
