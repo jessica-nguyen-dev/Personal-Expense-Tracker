5784e6dc02e8b7aab283d66ab5755f784c0ac413
jessica-nguyen-dev <jessica.u.nguyen@sjsu.edu>
2024-09-13 20:27:35 -0700

    Expense Class

---
+++ expense.py
@@ -0,0 +1,7 @@
+# defines a class named Expense, which is used to represent an expense with three attributes: name, category, and amount
+
+class Expense:      # __init__ constructor is automatically called when a new instance is created
+    def __init__(self, name, category, amount) -> None:     # None indicates that this method does not return a value
+        self.name = name       # Name, category, and amount are parameters that are passed when a new object is created
+        self.category = category
+        self.amount = amount
