6a1c18c6919f38a363d3b5e43f87d64f3b3bd358
jessica-nguyen-dev <jessica.u.nguyen@sjsu.edu>
2024-09-13 20:35:42 -0700

    Broken app into parts

---
+++ expense_tracker.py
@@ -0,0 +1,15 @@
+def main():
+    print(f"Running Expense Tracker!")  # checks that main is running properly
+
+    # Get user input for expense.
+
+    # Write their expense to a file.
+
+    # Read file and summarize expenses.
+
+    pass
+
+# ensures that the code runs only when we run this file, not when in other files
+if __name__ == "__main__":   # will only be true if we run the file directly
+    main()
+
